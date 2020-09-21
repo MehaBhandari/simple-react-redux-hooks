8dd1bb6b395e1cf62942ca5dc63e73a87e29f2fc
Meha <ahem4progs@gmail.com>
2020-09-21 18:25:29 +0530

    Updated UI template

---
+++ src/component/app.jsx
@@ -0,0 +1,29 @@
+import React, {useEffect} from 'react';
+import ReactDOM from 'react-dom';
+
+import {connect} from 'react-redux';
+import * as actions from '../action';
+import Axios from 'axios';
+
+function App(props) {
+    useEffect(() => {
+        Axios.get('http://5c055de56b84ee00137d25a0.mockapi.io/api/v1/employees').then((resp)=>{
+            props.setEmployeeList(resp.data);
+        })
+    },[])
+
+    return (
+        <>
+            {props.employees && (props.employees.map((emp)=>{
+                return(
+                    <div key={emp.id}>
+                        <img src={emp.avatar} alt={emp.name} />
+                        <h1>User Name: {emp.name}</h1>
+                        <h2>User created on {new Date(emp.createdAt).toString()}</h2>
+                        <h3>Employee Id: {emp.id}</h3>
+                    </div>
+                )
+            }))}
+        </>
+    )
+}
