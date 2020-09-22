a7cce781dab0ec84eeba09e8ffec5ca9cfab218f
Meha <ahem4progs@gmail.com>
2020-09-22 10:35:18 +0530

    resolved location of files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import {rootReducer} from 'rootReducer';
+import {rootReducer} from './reducer/index';
 import { AppComponent } from './component/app';
 
 var appStore = createStore(rootReducer);
